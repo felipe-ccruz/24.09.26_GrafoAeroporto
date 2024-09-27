5f740d441ba7b8bb62952cdd77af39db68133e00
Felipe Cruz <felipe23070305@aluno.cesupa.br>
2024-09-27 15:42:09 -0300

    Algoritimo Reduzido

--- src/App.java
+++ src/App.java
@@ -1,63 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Sao Paulo = a1
-        // Rio de Janeiro = a2
-        // Belo Horizonte = a3
-        // Brasilia = a4
-        // Salvador = a5
-        // Fortaleza = a6
-        // Manaus = a7
-        // Curitiba = a8
-        // Recife = a9
-        // Porto Alegre = a10
-        // Belem = a11
-        // Goiania = a12
-        // Sao Luis = a13
-        // Maceio = a14
-        // Natal = a15
-        // Teresina = a16
-        // Joao Pessoa = a17
-        // Aracaju = a18
-        // Campo Grande = a19
-        // Cuiaba = a20
-        // Florianopolis = a21
-        // Palmas = a22
-        // Rio Branco = a23
-        // Porto Velho = a24
-        // Boa Vista = a25
-        // Macapa = a26
-        // Vitoria = a27
+        // a1 - Manaus
+        // a2 - Belem
+        // a3 - Rio Branco
+        // a4 - Porto Velho
+        // a5 - Boa Vista
+        // a6 - Macapa
 
-
-        // Primeira rota
         Route r1 = new Route("Route-1");
-        r1.addStep(1015, "sao paulo", "belem");
-        r1.addStep(2853, "belem", "Cuiaba");
-        r1.addStep(2676, "Cuiaba", "Joao Pessoa");
-        r1.addStep(2794, "Joao Pessoa", "sao paulo");
+        r1.addStep(525, "manaus", "boa vista");   
+        r1.addStep(787, "boa vista", "belem");    
+        r1.addStep(1098, "belem", "macapa");      
+        r1.addStep(1130, "macapa", "manaus");     
 
         r1.printConnectMat();
         r1.printInfo();
 
-        // Segunda rota
         Route r2 = new Route("Route-2");
-        r2.addStep(870, "curitiba", "florianopolis");
-        r2.addStep(1254, "florianopolis", "porto alegre");
-        r2.addStep(2327, "porto alegre", "salvador");
-        r2.addStep(1443, "salvador", "curitiba");
+        r2.addStep(514, "porto velho", "rio branco");  
+        r2.addStep(1322, "rio branco", "manaus");      
+        r2.addStep(1297, "manaus", "belem");           
+        r2.addStep(1475, "belem", "porto velho");      
 
         r2.printConnectMat();
         r2.printInfo();
 
-        // Terceira rota
         Route r3 = new Route("Route-3");
-        r3.addStep(1578, "manaus", "fortaleza");
-        r3.addStep(1407, "fortaleza", "natal");
-        r3.addStep(1210, "natal", "brasilia");
-        r3.addStep(2090, "brasilia", "manaus");
+        r3.addStep(1475, "porto velho", "belem");      
+        r3.addStep(1098, "belem", "macapa");           
+        r3.addStep(787, "macapa", "boa vista");        
+        r3.addStep(525, "boa vista", "manaus");        
 
         r3.printConnectMat();
         r3.printInfo();
+
 
 
         
